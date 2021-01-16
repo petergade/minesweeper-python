a0ec1287a2af8237f185dd36deadf0acd8658ade
Karel Bém <karel.bem@gmail.com>
2021-01-16 16:59:10 +0100

    scale images

--- src/MineSweeper/mainscreen.py
+++ src/MineSweeper/mainscreen.py
@@ -6,8 +6,6 @@ from os import path
 from gamestate import GameState
 from sprites import Brick
 
-
-
 assets_folder = path.join(path.dirname(__file__), "assets")
 
 
@@ -19,25 +17,28 @@ class MainScreen:
         self.height: int = difficulty.height
         self.mines: int = difficulty.mines
         self.images = {}
-        self.sprites = pygame.sprite.Group()
+        self.bricks = pygame.sprite.Group()
 
     def load_images(self):
-        self.images[common.SpecialSquareValues.MINE] = pygame.image.load(path.join(assets_folder, "mina.png"))
-        self.images[common.SpecialSquareValues.FLAG] = pygame.image.load(path.join(assets_folder, "flag.png"))
-        self.images[common.SpecialSquareValues.FOG] = pygame.image.load(path.join(assets_folder, "pole.png"))
-        self.images[0] = pygame.image.load(path.join(assets_folder, "pole-odkryte.png"))
+        self.images[common.SpecialSquareValues.MINE] = pygame.image.load(path.join(assets_folder, "mina.png")).convert()
+        self.images[common.SpecialSquareValues.FLAG] = pygame.image.load(path.join(assets_folder, "flag.png")).convert()
+        self.images[common.SpecialSquareValues.FOG] = pygame.image.load(path.join(assets_folder, "pole.png")).convert()
+        self.images[0] = pygame.image.load(path.join(assets_folder, "pole-odkryte.png")).convert()
         for i in range(8):
-            self.images[i + 1] = pygame.image.load(path.join(assets_folder, f"pole-{i + 1}.png"))
+            self.images[i + 1] = pygame.image.load(path.join(assets_folder, f"pole-{i + 1}.png")).convert()
+
+    def scale_images(self):
+        for key in self.images.keys():
+            self.images[key] = pygame.transform.scale(self.images[key], (common.SQ_SIZE, common.SQ_SIZE))
+
 
     def show(self) -> common.GameResult:
         # TODO: podle width a height urcit rozmery obrazovky a kosticky
         # TODO: nacteni grafiky, zvuků, hudby, animací
 
-        #player = Spritesheet(path.join(folder, ""))
-        #folder = path.dirname(_file_)
-
         # Grafika
         self.load_images()
+        self.scale_images()
 
         # Zvuky, hudba, animace
         explosion_sound = pygame.mixer.Sound(path.join(assets_folder, "vybuch.mp3"))
@@ -73,11 +74,12 @@ class MainScreen:
                         self.hide_board = not self.hide_board
 
             # update
-            self.sprites.update()
+            self.update_bricks()
+            self.bricks.update()
 
             # render
-            screen.fill(common.WHITE)
-            self.sprites.draw(screen)
+            #screen.fill(common.WHITE)
+            self.bricks.draw(screen)
             pygame.display.flip()
 
             if self.game_state.game_result_type == common.GameResultType.LOST or \
@@ -89,18 +91,16 @@ class MainScreen:
     def initialize_sprites(self):
         for r in range(self.height):
             for c in range(self.width):
-                brick = Brick(r * common.SQ_SIZE, c * common.SQ_SIZE, self.images)
-                self.sprites.add(brick)
+                brick = Brick(r, c, self.images)
+                self.bricks.add(brick)
 
-    def draw_game_state(self, screen: pygame.Surface) -> None:
+    def update_bricks(self) -> None:
         if self.game_state.game_result_type == common.GameResultType.LOST or self.game_state.game_result_type == common.GameResultType.WIN:
             map_to_draw = self.game_state.game_map
         else:
             map_to_draw = self.game_state.player_map if self.hide_board else self.game_state.game_map
-        for r in range(self.height):
-            for c in range(self.width):
-                text = self.get_text_to_draw(map_to_draw[r][c])
-                brick = Brick(self.images[common.SpecialSquareValues.FOG])
+        for brick in self.bricks:
+            brick.value = map_to_draw[brick.r][brick.c]
 
 
     @staticmethod
--- src/MineSweeper/sprites.py
+++ src/MineSweeper/sprites.py
@@ -1,20 +1,23 @@
 import pygame
-from common import SpecialSquareValues
+from common import SpecialSquareValues, SQ_SIZE
 
 
 class Brick(pygame.sprite.Sprite):
-    def __init__(self, x, y, images):
+    def __init__(self, r, c, images):
         pygame.sprite.Sprite.__init__(self)
+        self.r = r
+        self.c = c
         self.images = images
         self.image = images[SpecialSquareValues.FOG]
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.rect.x = r * SQ_SIZE
+        self.rect.y = c * SQ_SIZE
         self.value = SpecialSquareValues.FOG
 
     def update(self) -> None:
         center = self.rect.center
         self.image = self.images[self.value]
+        print(self.value)
         self.rect = self.image.get_rect()
         self.rect.center = center
 
